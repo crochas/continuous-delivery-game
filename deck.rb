ef3e95afdd28afde953b879bf1ae0fd4c7950e1f
François Lecomte <francois.lecomte@gmail.com>
2017-10-27 16:40:24 +0200

    rules

--- deck.rb
+++ deck.rb
@@ -74,6 +74,7 @@ Cards2 = yaml2dataframe(Cards)
 make_cards(Cards2.description_fr, "cards_fr.pdf")
 make_cards(Cards2.description_en, "cards_en.pdf")
 
+# levels
 LevelCards = YAML.load_file('data/levels.yml')
 LevelCards2 = yaml2dataframe(LevelCards)
 
@@ -87,6 +88,24 @@ Squib::Deck.new(cards: LevelCards.size, layout: 'layout-cards.yml') do
   text str: LevelCards2.title, layout: 'LTitle', color: "white"
   png file: LevelCards2.icon, x: 150, y: 340, width: 540, height: 690
 
-
   save_home_made "levels.pdf"
 end
+
+# rules
+def make_rules_cards(description, output_file)
+  Squib::Deck.new(cards: 8, layout: 'layout-cards.yml') do
+    background color: 'white'
+    rect layout: 'cut' # cut line as defined by TheGameCrafter
+    rect layout: 'safe' # safe zone as defined by TheGameCrafter
+    text str: RuleCards['title'], layout: 'LTitle', height: 120, color: 'black', font_size: 30
+    text str: description, layout: 'Description', justify: true, y: 230, height: 800, font_size: 28, align: 'left'
+
+    save_home_made output_file
+  end
+end
+
+RuleCards = YAML.load_file('data/rules.yml')
+
+make_rules_cards(RuleCards['description_fr'], "rules_fr.pdf")
+make_rules_cards(RuleCards['description_en'], "rules_en.pdf")
+
